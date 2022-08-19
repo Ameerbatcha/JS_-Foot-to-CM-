669850adfc15f346da6a53ab78f74182044700ba
Ameer Batcha <ameeruvaise@gmail.com>
2022-08-19 10:49:56 +0530

    initial commit of foot to cm

---
+++ jsscript.js
@@ -0,0 +1,27 @@
+
+document.getElementById("btnsubmit").onmousedown = function () {
+
+    // declaring variable with entered values
+    var infoot = document.getElementById("idvalue").value;
+    let incm = document.getElementById("answer");
+
+    //condition checking withmfoot value
+    if (infoot < 0) {
+        incm.innerHTML = "Please enter a feet value >0";
+    }
+    else if (infoot >= 0) {
+        incm.innerHTML = feettocm(infoot);
+
+    }
+    else if (typeof infoot === 'string') {
+        incm.innerHTML = "Please enter a valid number!"; 
+
+    }
+
+    //feet to cm conversion function
+    function feettocm(x) {
+        x *= 30.48;
+        return x + " cm";
+    }
+
+}
